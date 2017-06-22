9a8db0d4e2fb22d322e23850e18f10f80e81f0e3
a_liYa <liyaf@8531.cn>
2017-06-22 20:04:16 +0800

    初始化项目

---
+++ app/src/main/java/com/a_liya/uimode/demo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.a_liya.uimode.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.a_liya.uimode.demo.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
