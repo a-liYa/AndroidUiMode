fa13823e4b708469824c6503aaa7bc176d80ef43
a_liYa <liyaf@8531.cn>
2017-06-26 00:37:57 +0800

    添加UiMode的divider属性的支持

--- library/src/main/java/com/a_liya/uimode/mode/UiListView.java
+++ library/src/main/java/com/a_liya/uimode/mode/UiListView.java
@@ -1,31 +1,32 @@
 package com.a_liya.uimode.mode;
 
 import android.content.res.Resources;
+import android.graphics.drawable.ColorDrawable;
 import android.support.annotation.CallSuper;
 import android.support.v4.content.ContextCompat;
 import android.util.SparseArray;
 import android.util.TypedValue;
-import android.widget.TextView;
+import android.widget.ListView;
 
 import com.a_liya.uimode.R;
 import com.a_liya.uimode.utils.CheckUtils;
 
 /**
- * TextView{@link android.widget.TextView} 的 UiMode {@link UiMode}实现类
+ * ListView{@link android.widget.ListView} 的 UiMode {@link UiMode}实现类
  *
  * @author a_liYa
- * @date 2017/6/25 13:37.
+ * @date 2017/6/26 00:09.
  */
-public class UiTextView<T extends TextView> extends UiView<T> {
+public class UiListView<T extends ListView> extends UiView<T> {
 
-    protected int attrIdTextColor;
+    protected int attrIdDivider;
 
     @CallSuper
     @Override
     public void assign(SparseArray<Integer> attrs) {
         super.assign(attrs);
         if (attrs != null) {
-            attrIdTextColor = attrs.get(R.styleable.SupportUiMode_android_textColor, NO_ATTR_ID);
+            attrIdDivider = attrs.get(R.styleable.SupportUiMode_android_divider, NO_ATTR_ID);
         }
     }
 
@@ -33,22 +34,26 @@ public class UiTextView<T extends TextView> extends UiView<T> {
     @Override
     public <V extends T> void apply(V v, Resources.Theme theme) {
         super.apply(v, theme);
-        if (CheckUtils.residValid(attrIdTextColor)) {
-            theme.resolveAttribute(attrIdTextColor, sOutValue, true);
+        if (v == null || theme == null) return;
+
+        if (CheckUtils.residValid(attrIdDivider)) {
+            int dividerHeight = v.getDividerHeight();
+            theme.resolveAttribute(attrIdDivider, sOutValue, true);
+
             switch (sOutValue.type) {
                 case TypedValue.TYPE_INT_COLOR_ARGB4:
                 case TypedValue.TYPE_INT_COLOR_ARGB8:
                 case TypedValue.TYPE_INT_COLOR_RGB4:
                 case TypedValue.TYPE_INT_COLOR_RGB8:
-                    v.setTextColor(sOutValue.data);
+                    v.setDivider(new ColorDrawable(sOutValue.data));
                     break;
                 case TypedValue.TYPE_STRING:
-                    v.setTextColor(ContextCompat.
-                            getColorStateList(v.getContext(), sOutValue.resourceId));
+                    v.setDivider(ContextCompat.getDrawable(v.getContext(), sOutValue.resourceId));
                     break;
-
-
             }
+
+            v.setDividerHeight(dividerHeight);
         }
+
     }
 }
