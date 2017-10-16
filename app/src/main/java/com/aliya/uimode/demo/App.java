6c53958f0285a26fd1ca51b4170dae21dd6a8e40
a_liYa <liyaf@8531.cn>
2017-10-16 21:25:53 +0800

    示例App添加LeakCanary检测内存泄漏

--- app/src/main/java/com/aliya/uimode/demo/App.java
+++ app/src/main/java/com/aliya/uimode/demo/App.java
@@ -3,6 +3,7 @@ package com.aliya.uimode.demo;
 import android.app.Application;
 
 import com.aliya.uimode.UiModeManager;
+import com.squareup.leakcanary.LeakCanary;
 
 /**
  * Application
@@ -19,6 +20,13 @@ public class App extends Application {
         ThemeMode.init(this);
         ThemeMode.initTheme(R.style.AppTheme, R.style.NightAppTheme);
         UiModeManager.init(this, R.styleable.SupportUiMode);
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
 
     }
 
