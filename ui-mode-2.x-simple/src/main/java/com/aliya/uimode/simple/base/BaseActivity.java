9c0b7cca9b327fd819fa0f51b0ba1378b6b6311f
a_liYa <liyaf@8531.cn>
2018-04-12 10:32:07 +0800

    更新示例BaseActivity实现UiModeChangeListener接口

--- ui-mode-2.x-simple/src/main/java/com/aliya/uimode/simple/base/BaseActivity.java
+++ ui-mode-2.x-simple/src/main/java/com/aliya/uimode/simple/base/BaseActivity.java
@@ -3,8 +3,10 @@ package com.aliya.uimode.simple.base;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.aliya.uimode.UiModeManager;
+import com.aliya.uimode.intef.UiModeChangeListener;
 
 /**
  * Base Activity
@@ -12,11 +14,17 @@ import com.aliya.uimode.UiModeManager;
  * @author a_liYa
  * @date 2018/2/1 15:05.
  */
-public class BaseActivity extends AppCompatActivity {
+public class BaseActivity extends AppCompatActivity implements UiModeChangeListener {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         UiModeManager.setInflaterFactor(getLayoutInflater());
         super.onCreate(savedInstanceState);
     }
+
+    @Override
+    public void onUiModeChange() {
+        Log.e("TAG","onUiModeChange " + this);
+    }
+
 }
