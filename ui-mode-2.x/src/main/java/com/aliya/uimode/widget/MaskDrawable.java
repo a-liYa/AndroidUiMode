8c7dc2fb9b9ba3d6f0bec7d8757f1a279bfcc5f6
a_liYa <liyaf@8531.cn>
2020-05-29 00:08:47 +0800

    优化 RadioButton drawableLeft 属性设置 RadioButton#setChecked(boolean) 在Android 6.0 及以下无效问题

--- ui-mode-2.x/src/main/java/com/aliya/uimode/widget/MaskDrawable.java
+++ ui-mode-2.x/src/main/java/com/aliya/uimode/widget/MaskDrawable.java
@@ -1,26 +1,17 @@
 package com.aliya.uimode.widget;
 
 import android.content.res.ColorStateList;
-import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.ColorFilter;
-import android.graphics.Outline;
 import android.graphics.PorterDuff;
 import android.graphics.Rect;
 import android.graphics.Region;
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.support.annotation.RequiresApi;
-import android.util.AttributeSet;
+import android.support.v4.graphics.drawable.DrawableCompat;
 
 import com.aliya.uimode.intef.UiModeChangeListener;
-
-import org.xmlpull.v1.XmlPullParser;
-import org.xmlpull.v1.XmlPullParserException;
-
-import java.io.IOException;
 
 /**
  * 遮罩 Drawable 包装类
@@ -28,14 +19,18 @@ import java.io.IOException;
  * @author a_liYa
  * @date 2018/11/9 17:16.
  */
-public class MaskDrawable extends Drawable implements UiModeChangeListener {
+public class MaskDrawable extends Drawable implements UiModeChangeListener, Drawable.Callback {
 
     private Drawable mDrawable;
     private MaskHelper mMaskHelper;
 
     public MaskDrawable(Drawable drawable, MaskHelper maskHelper) {
-        mDrawable = drawable;
         mMaskHelper = maskHelper;
+
+        this.mDrawable = drawable;
+        if (drawable != null) {
+            drawable.setCallback(this);
+        }
     }
 
     @Override
@@ -58,296 +53,133 @@ public class MaskDrawable extends Drawable implements UiModeChangeListener {
         mMaskHelper.resolveRealMaskColor();
     }
 
-    @Override
-    public void setAlpha(int alpha) {
-        mDrawable.setAlpha(alpha);
+    protected void onBoundsChange(Rect bounds) {
+        this.mDrawable.setBounds(bounds);
     }
 
-    @Override
-    public void setColorFilter(@Nullable ColorFilter colorFilter) {
-        mDrawable.setColorFilter(colorFilter);
-    }
-
-    @Override
-    public int getOpacity() {
-        return mDrawable.getOpacity();
-    }
-
-    @Override
-    public void setBounds(int left, int top, int right, int bottom) {
-        mDrawable.setBounds(left, top, right, bottom);
-        super.setBounds(left, top, right, bottom);
-    }
-
-    @Override
-    public void setBounds(@NonNull Rect bounds) {
-        mDrawable.setBounds(bounds);
-        super.setBounds(bounds);
-    }
-
-    @NonNull
-    @Override
-    public Rect getDirtyBounds() {
-        return mDrawable.getBounds();
-    }
-
-    @Override
     public void setChangingConfigurations(int configs) {
-        mDrawable.setChangingConfigurations(configs);
-        super.setChangingConfigurations(configs);
+        this.mDrawable.setChangingConfigurations(configs);
     }
 
-    @Override
     public int getChangingConfigurations() {
-        return mDrawable.getChangingConfigurations();
+        return this.mDrawable.getChangingConfigurations();
     }
 
-    @Override
     public void setDither(boolean dither) {
-        mDrawable.setDither(dither);
-        super.setDither(dither);
+        this.mDrawable.setDither(dither);
     }
 
-    @Override
     public void setFilterBitmap(boolean filter) {
-        mDrawable.setFilterBitmap(filter);
-        super.setFilterBitmap(filter);
+        this.mDrawable.setFilterBitmap(filter);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public boolean isFilterBitmap() {
-        return mDrawable.isFilterBitmap();
+    public void setAlpha(int alpha) {
+        this.mDrawable.setAlpha(alpha);
     }
 
-    @Nullable
-    @Override
-    public Callback getCallback() {
-        return mDrawable.getCallback();
+    public void setColorFilter(ColorFilter cf) {
+        this.mDrawable.setColorFilter(cf);
     }
 
-    @Override
-    public void invalidateSelf() {
-        mDrawable.invalidateSelf();
-        super.invalidateSelf();
-    }
-
-    @Override
-    public void scheduleSelf(@NonNull Runnable what, long when) {
-        mDrawable.scheduleSelf(what, when);
-        super.scheduleSelf(what, when);
-    }
-
-    @Override
-    public void unscheduleSelf(@NonNull Runnable what) {
-        mDrawable.unscheduleSelf(what);
-        super.unscheduleSelf(what);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public int getLayoutDirection() {
-        return mDrawable.getLayoutDirection();
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public boolean onLayoutDirectionChanged(int layoutDirection) {
-        return mDrawable.onLayoutDirectionChanged(layoutDirection);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    @Override
-    public int getAlpha() {
-        return mDrawable.getAlpha();
-    }
-
-    @Override
-    public void setColorFilter(int color, @NonNull PorterDuff.Mode mode) {
-        mDrawable.setColorFilter(color, mode);
-        super.setColorFilter(color, mode);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    @Override
-    public void setTint(int tintColor) {
-        mDrawable.setTint(tintColor);
-        super.setTint(tintColor);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    @Override
-    public void setTintList(@Nullable ColorStateList tint) {
-        mDrawable.setTintList(tint);
-        super.setTintList(tint);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    @Override
-    public void setTintMode(@NonNull PorterDuff.Mode tintMode) {
-        mDrawable.setTintMode(tintMode);
-        super.setTintMode(tintMode);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    @Nullable
-    @Override
-    public ColorFilter getColorFilter() {
-        return mDrawable.getColorFilter();
-    }
-
-    @Override
-    public void clearColorFilter() {
-        mDrawable.clearColorFilter();
-        super.clearColorFilter();
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    @Override
-    public void setHotspot(float x, float y) {
-        mDrawable.setHotspot(x, y);
-        super.setHotspot(x, y);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    @Override
-    public void setHotspotBounds(int left, int top, int right, int bottom) {
-        mDrawable.setHotspotBounds(left, top, right, bottom);
-        super.setHotspotBounds(left, top, right, bottom);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.M)
-    @Override
-    public void getHotspotBounds(@NonNull Rect outRect) {
-        mDrawable.getHotspotBounds(outRect);
-    }
-
-    @Override
     public boolean isStateful() {
-        return mDrawable.isStateful();
+        return this.mDrawable.isStateful();
     }
 
-    @Override
-    public boolean setState(@NonNull int[] stateSet) {
-        return mDrawable.setState(stateSet);
+    public boolean setState(int[] stateSet) {
+        if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.M) {
+            // 解决Android 6.0 及以下版本，不能 invalidateSelf()
+            if (mDrawable.getCallback() != this) {
+                mDrawable.setCallback(this);
+            }
+        }
+        return this.mDrawable.setState(stateSet);
     }
 
-    @NonNull
-    @Override
     public int[] getState() {
-        return mDrawable.getState();
+        return this.mDrawable.getState();
     }
 
-    @Override
     public void jumpToCurrentState() {
-        mDrawable.jumpToCurrentState();
-        super.jumpToCurrentState();
+        DrawableCompat.jumpToCurrentState(this.mDrawable);
     }
 
-    @NonNull
-    @Override
     public Drawable getCurrent() {
-        return super.getCurrent();
+        return this.mDrawable.getCurrent();
     }
 
-    @Override
     public boolean setVisible(boolean visible, boolean restart) {
-        super.setVisible(visible, restart);
-        return mDrawable.setVisible(visible, restart);
+        return super.setVisible(visible, restart) || this.mDrawable.setVisible(visible, restart);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    @Override
-    public void setAutoMirrored(boolean mirrored) {
-        mDrawable.setAutoMirrored(mirrored);
-        super.setAutoMirrored(mirrored);
+    public int getOpacity() {
+        return this.mDrawable.getOpacity();
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
-    @Override
-    public boolean isAutoMirrored() {
-        return mDrawable.isAutoMirrored();
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    @Override
-    public void applyTheme(@NonNull Resources.Theme t) {
-        mDrawable.applyTheme(t);
-        super.applyTheme(t);
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    @Override
-    public boolean canApplyTheme() {
-        return mDrawable.canApplyTheme();
-    }
-
-    @Nullable
-    @Override
     public Region getTransparentRegion() {
-        return mDrawable.getTransparentRegion();
+        return this.mDrawable.getTransparentRegion();
     }
 
-    @Override
     public int getIntrinsicWidth() {
-        return mDrawable.getIntrinsicWidth();
+        return this.mDrawable.getIntrinsicWidth();
     }
 
-    @Override
     public int getIntrinsicHeight() {
-        return mDrawable.getIntrinsicHeight();
+        return this.mDrawable.getIntrinsicHeight();
     }
 
-    @Override
     public int getMinimumWidth() {
-        return mDrawable.getMinimumWidth();
+        return this.mDrawable.getMinimumWidth();
     }
 
-    @Override
     public int getMinimumHeight() {
-        return mDrawable.getMinimumHeight();
+        return this.mDrawable.getMinimumHeight();
     }
 
-    @Override
-    public boolean getPadding(@NonNull Rect padding) {
-        super.getPadding(padding);
-        return mDrawable.getPadding(padding);
+    public boolean getPadding(Rect padding) {
+        return this.mDrawable.getPadding(padding);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    @Override
-    public void getOutline(@NonNull Outline outline) {
-        mDrawable.getOutline(outline);
-        super.getOutline(outline);
+    public void invalidateDrawable(Drawable who) {
+        this.invalidateSelf();
     }
 
-    @NonNull
-    @Override
-    public Drawable mutate() {
-        mDrawable.mutate();
-        return super.mutate();
+    public void scheduleDrawable(Drawable who, Runnable what, long when) {
+        this.scheduleSelf(what, when);
     }
 
-    @Override
-    public void inflate(@NonNull Resources r, @NonNull XmlPullParser parser, @NonNull
-            AttributeSet attrs) throws XmlPullParserException, IOException {
-        mDrawable.inflate(r, parser, attrs);
-        super.inflate(r, parser, attrs);
+    public void unscheduleDrawable(Drawable who, Runnable what) {
+        this.unscheduleSelf(what);
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
-    @Override
-    public void inflate(@NonNull Resources r, @NonNull XmlPullParser parser, @NonNull
-            AttributeSet attrs, @Nullable Resources.Theme theme) throws XmlPullParserException,
-            IOException {
-        mDrawable.inflate(r, parser, attrs, theme);
-        super.inflate(r, parser, attrs, theme);
+    protected boolean onLevelChange(int level) {
+        return this.mDrawable.setLevel(level);
     }
 
-    @Nullable
-    @Override
-    public ConstantState getConstantState() {
-        return mDrawable.getConstantState();
+    public void setAutoMirrored(boolean mirrored) {
+        DrawableCompat.setAutoMirrored(this.mDrawable, mirrored);
     }
 
+    public boolean isAutoMirrored() {
+        return DrawableCompat.isAutoMirrored(this.mDrawable);
+    }
+
+    public void setTint(int tint) {
+        DrawableCompat.setTint(this.mDrawable, tint);
+    }
+
+    public void setTintList(ColorStateList tint) {
+        DrawableCompat.setTintList(this.mDrawable, tint);
+    }
+
+    public void setTintMode(PorterDuff.Mode tintMode) {
+        DrawableCompat.setTintMode(this.mDrawable, tintMode);
+    }
+
+    public void setHotspot(float x, float y) {
+        DrawableCompat.setHotspot(this.mDrawable, x, y);
+    }
+
+    public void setHotspotBounds(int left, int top, int right, int bottom) {
+        DrawableCompat.setHotspotBounds(this.mDrawable, left, top, right, bottom);
+    }
 }
