75a2f7dbb8ccad0f81b71f39b09ff17acd45faff
liyafei <11131669@bbktel.com>
2021-04-02 14:53:26 +0800

    添加监听系统深色模式切换事件

--- ui-mode-2.x-sample/src/main/java/com/aliya/uimode/sample/App.java
+++ ui-mode-2.x-sample/src/main/java/com/aliya/uimode/sample/App.java
@@ -2,6 +2,9 @@ package com.aliya.uimode.sample;
 
 import android.app.Application;
 import android.content.res.Configuration;
+import android.database.ContentObserver;
+import android.os.Handler;
+import android.provider.Settings;
 import android.util.Log;
 
 import androidx.appcompat.app.AppCompatDelegate;
@@ -14,6 +17,8 @@ import androidx.appcompat.app.AppCompatDelegate;
  */
 public class App extends Application {
 
+    private final String PATH_VIVO_UI_MODE = "vivo_nightmode_used";
+
     @Override
     public void onCreate() {
         super.onCreate();
@@ -21,6 +26,21 @@ public class App extends Application {
         Log.e("TAG", "App - defaultNightMode: " + AppCompatDelegate.getDefaultNightMode());
 
         AppUiMode.init(App.this);
+
+        registerUiModeObserver();
+    }
+
+    private void registerUiModeObserver() {
+        getContentResolver().registerContentObserver(
+                Settings.System.getUriFor(PATH_VIVO_UI_MODE), false,
+                new ContentObserver(new Handler()) {
+                    @Override
+                    public void onChange(boolean selfChange) {
+                        int uiMode = Settings.System.getInt(getContentResolver(), PATH_VIVO_UI_MODE, -2);
+                        // uiMode = 1 时代表深色模式开启
+                        Log.e("TAG", "onChange: " + selfChange + " - " + uiMode);
+                    }
+                });
     }
 
     public static String uiModeToString(int uiMode) {
